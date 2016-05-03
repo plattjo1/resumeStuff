bbeeef2364424a2c3a36ee7c5f578e4d808608a6
plattjo1 <plattjo1@msu.edu>
2016-05-02 20:07:01 -0700

    commit 16
    commit 16

--- public/old stuff/otherJavaScript.js
+++
@@ -1,7 +0,0 @@
-$('#the-thing-that-opens-your-alert').click(function () {
-  $('#le-alert').addClass('in'); // shows alert with Bootstrap CSS3 implem
-});
-
-$('.close').click(function () {
-  $(this).parent().removeClass('in'); // hides alert with Bootstrap CSS3 implem
-});
